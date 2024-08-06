d11ece4bbd191309acc183d19c6ca4c50490a16c
Mike Vays <michailvays@gmail.com>
2024-08-06 16:55:10 +0700

    Implemented two ways to read parameters from GET requests

--- springMVCpractise/src/main/java/com/practise/MVC/springMVCpractise/controllers/ControllerClass.java
+++ springMVCpractise/src/main/java/com/practise/MVC/springMVCpractise/controllers/ControllerClass.java
@@ -1,20 +1,26 @@
 package com.practise.MVC.springMVCpractise.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/messages")
 public class ControllerClass {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", defaultValue = "User!") String name, Model model) {
+        model.addAttribute("name", name);
         return "helloPage";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage() {
+    public String goodbyePage(HttpServletRequest request, Model model) {
+        String name = request.getParameter("name");
+        model.addAttribute("name", name);
         return "goodbyePage";
     }
 }
