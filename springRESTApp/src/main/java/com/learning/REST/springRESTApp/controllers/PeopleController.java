572bf4275357e2c2c779c0a814b77e4f1a27d83f
Mike Vays <michailvays@gmail.com>
2024-08-07 16:05:34 +0700

    Added CREATE method

--- springRESTApp/src/main/java/com/learning/REST/springRESTApp/controllers/PeopleController.java
+++ springRESTApp/src/main/java/com/learning/REST/springRESTApp/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package com.learning.REST.springRESTApp.controllers;
 
 import com.learning.REST.springRESTApp.dao.PersonDAO;
+import com.learning.REST.springRESTApp.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -28,6 +27,17 @@ public class PeopleController {
     public String personById(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.getPersonById(id));
         return "personPage";
+    }
+
+    @GetMapping("/new")
+    public String createPerson(@ModelAttribute("person") Person person) {
+        return "createPersonForm";
+    }
+
+    @PostMapping()
+    public String addPersonToListOfPeople(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 
 }
